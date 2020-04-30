765e88974fd8ce4f73bd3ea21924fb5f0cd2c635
santiracca <santiagoracca7@gmail.com>
2020-04-30 14:50:19 -0400

    Add typescript with google maps

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,39 @@
-// Code goes here!
+import axios from "axios";
+
+const form = document.querySelector("form");
+const addressInput = document.getElementById("address")! as HTMLInputElement;
+
+const GOOGLE_API_KEY = "AIzaSyCjKmjqlHCszDKmobQ2yLYiH4YRRpgLzNo";
+
+type GoolgeGeocodingResponse = {
+  results: { geometry: { location: { lat: number; lng: number } } }[];
+  status: "OK" | "ZERO_RESULTS";
+};
+
+function searchAddressHandler(event: Event) {
+  event.preventDefault();
+  const enteredAddress = addressInput.value;
+
+  const placesUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURI(
+    enteredAddress
+  )}&key=${GOOGLE_API_KEY}`;
+  axios
+    .get<GoolgeGeocodingResponse>(placesUrl)
+    .then((response) => {
+      if (response.data.status !== "OK") {
+        throw new Error("Could not fetch location");
+      }
+      const coordinates = response.data.results[0].geometry.location;
+
+      const map = new google.maps.Map(document.getElementById("map")!, {
+        center: coordinates,
+        zoom: 16,
+      });
+      new google.maps.Marker({ position: coordinates, map: map });
+    })
+    .catch((err) => {
+      alert(err.message);
+    });
+}
+
+form!.addEventListener("submit", searchAddressHandler);
